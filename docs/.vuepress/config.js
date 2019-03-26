9eb64141c5b464aae810cc1f6aeab4e52bbb8071
superbiger <superbiger@qq.com>
2019-03-26 11:42:39 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: '@superbiger/vuepress-plugin-tabs',
+    description: "markdown custom container to display content in tabs",
+    themeConfig: {
+        repo: "https://github.com/superbiger/vuepress-plugin-tabs",
+        editLinks: true,
+        docsDir: 'docs',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated'
+    },
+    plugins: [
+        require('../../index'),
+        [
+            '@vuepress/pwa', {
+                serviceWorker: true,
+                updatePopup: {
+                message: "New content is available.",
+                buttonText: "Refresh"
+            }
+        }]
+    ]
+}
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = (options, context) => ({
-    
-})
+const path = require('path')
+
+module.exports = {
+    enhanceAppFiles: [
+        path.resolve(__dirname, './lib/client.js')
+    ],
+
+    chainMarkdown (config) {
+        config
+            .plugin('@superbiger/tabs')
+            .use(require('./lib/markdownItPlugin'))
+    }
+}
